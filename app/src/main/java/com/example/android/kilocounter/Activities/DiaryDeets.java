46c0cb6ddb60a941d64c0d0faf2a799ea8dc0a60
TumoM <tumomasire@gmail.com>
2019-07-19 08:58:01 +0200

    Starting the Diary Details screen today.

--- app/src/main/java/com/example/android/kilocounter/Activities/DiaryDeets.java
+++ app/src/main/java/com/example/android/kilocounter/Activities/DiaryDeets.java
@@ -1,7 +1,10 @@
 package com.example.android.kilocounter.Activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.android.kilocounter.R;
 
@@ -11,5 +14,14 @@ public class DiaryDeets extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary_deets);
+
+        Context mContext = this;
+    }
+
+    public void launchCalcClick(View view){
+        Intent intent = new Intent(this,CalcActivity.class);
+
+        intent.putExtra("movie", "N/A");
+        this.startActivity(intent);
     }
 }
